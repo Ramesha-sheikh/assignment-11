d8303f3194696a7a6349ba581fe0392c2c745d7e
Ramesha javed <165018589+Ramesha-sheikh@users.noreply.github.com>
2024-12-16 16:09:22 +0500

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Link from 'next/link';
+
+interface NavData {
+  logo: string;
+  list: { text: string; href: string }[]; // Array of objects with text and href for links
+  button: string;
+}
+
+const Navbar = ({ logo, list = [], button }: NavData) => {
+  return (
+    <div>
+      <header className="bg-black grid grid-cols-[1fr,auto,1fr] items-center text-white px-8 py-4 gap-8">
+        {/* Logo Section */}
+        <div className="text-start">
+          <h1 className="text-xl font-bold text-orange-400">{logo}</h1> {/* Logo styled */}
+        </div>
+
+        {/* Navigation Section */}
+        <nav>
+          <ul className="grid grid-cols-4 gap-4 text-center">
+            {list.map((link, index) => (
+              <li key={index} className="hover:text-gray-400">
+                <Link href={link.href} className="text-lg">
+                {link.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+       
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
